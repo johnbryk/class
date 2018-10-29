3bf0ddf56169c0c9892f05a5783afa81400ea63a
John Bryk <jbryk@missporters.org>
2018-10-29 08:27:41 -0400

    new

--- 2018-10-29/Example1.java
+++ 2018-10-29/Example1.java
@@ -4,4 +4,13 @@
 public class Example1
 {
     
+    public static void printSquare(int n) {
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                System.out.print("X");
+            }
+            System.out.print("\n");
+        }
+    }
+    
 }
