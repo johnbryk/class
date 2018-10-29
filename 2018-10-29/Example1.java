d1aa492dd75dcd0cd765240b17d512f86bf0e940
John Bryk <jbryk@missporters.org>
2018-10-29 08:42:05 -0400

    Update 2018-10-29/Example1.java

--- 2018-10-29/Example1.java
+++ 2018-10-29/Example1.java
@@ -6,11 +6,28 @@ public class Example1
     
     public static void printSquare(int n) {
         for (int j = 0; j < n; j++) {
-            for (int i = 0; i < n; i++) {
-                System.out.print("X");
-            }
-            System.out.print("\n");
+            printRow(n);
         }
     }
+    
+    public static void printRectangle(int height, int width) {
+        for (int j = 0; j < height; j++) {
+            printRow(width);
+        }
+    }
+    
+    public static void printTriangle(int height) {
+        for (int j = 0; j < height; j++) {
+            printRow(j + 1);
+        }
+    }
+    
+    public static void printRow(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("X");
+        }
+        System.out.print("\n");
+    }
+    
     
 }
